e22f3125b31b566fe3eec78010d746356adc26bc
Tinport <122801701+Tinport@users.noreply.github.com>
2026-04-04 02:16:31 +0300

    Update Endotherma.java

--- src/example/Endotherma.java
+++ src/example/Endotherma.java
@@ -6,15 +6,12 @@ import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-import endotherma.content.EndBlocks
+import endotherma.content.EndBlocks;
 
 public class Endotherma extends Mod{
 
-    public Endotherma(){
-        Log.info("drugs 2");
-
     @Override
-    public void loadContent(){
+    public void loadContent() {
         Log.info("drugs");
         EndBlocks.load();
     }
