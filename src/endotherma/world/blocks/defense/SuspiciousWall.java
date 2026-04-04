4b1e30a7dbfb85d3ce83bbbfa559aa03a15d4705
Tinport <122801701+Tinport@users.noreply.github.com>
2026-04-04 23:31:47 +0300

    f

--- src/endotherma/world/blocks/defense/SuspiciousWall.java
+++ src/endotherma/world/blocks/defense/SuspiciousWall.java
@@ -5,16 +5,41 @@ import arc.audio.*;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
+import arc.struct.*;
 import arc.util.*;
+import arc.util.io.*;
 import mindustry.entities.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.world.*;
 import mindustry.world.meta.*;
+import mindustry.annotations.Annotations.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.logic.*;
+import mindustry.type.*;
+import mindustry.ui.*;
+import mindustry.world.blocks.heat.*;
+
 
 import static mindustry.Vars.*;
 
 public class SuspiciousWall extends Wall {
+
+    public SuspiciousWall(String name){
+        super(name);
+        solid = true;
+        destructible = true;
+        group = BlockGroup.walls;
+        buildCostMultiplier = 6f;
+        canOverdrive = false;
+        drawDisabled = false;
+        crushDamageMultiplier = 5f;
+        priority = TargetPriority.wall;
+
+        //it's a wall of course it's supported everywhere
+        envEnabled = Env.any;
+    }
     @Override
     public boolean configTapped(){
         configure(!enabled);
