b84ad19086ba64dba8a5b9aef8bb3aa7c583fc20
Tinport <122801701+Tinport@users.noreply.github.com>
2026-04-04 23:12:50 +0300

    this will corrupt my computer

---
+++ src/endotherma/world/blocks/defense/SuspiciousWall.java
@@ -0,0 +1,34 @@
+package endotherma.world.blocks.defense
+
+import arc.*;
+import arc.audio.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.util.*;
+import mindustry.entities.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+
+public class SuspiciousWall extends Wall {
+    @Override
+    public boolean configTapped(){
+        configure(!enabled);
+        clickSound.at(this);
+        return false;
+    }
+    @Override
+    public void draw(){
+        super.draw();
+
+        if(enabled){
+            Events.fire(Trigger.thoriumReactorOverheat);
+            kill();
+        }
+    }
+
+}
